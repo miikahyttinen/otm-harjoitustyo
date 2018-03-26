68aae1889498b1879cf029677a88aac60d11c49a
Miika Hyttinen <hyttinenmiika@gmail.com>
2018-03-26 18:48:27 +0300

    Testi metodeja lisätty maksukortille

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -12,6 +12,34 @@ public class MaksukorttiTest {
     public void setUp() {
         kortti = new Maksukortti(10);
     }
+    
+    public void saldoOikeinAlussa(){
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
+    
+    public void saldonLisaysToimii(){
+        kortti.lataaRahaa(10);
+        assertEquals("Kortilla on rahaa 20.0 euroa", kortti.toString());
+    }
+    
+    public void saldonOttoToimii(){
+        kortti.otaRahaa(9);
+        assertEquals("Kortilla on rahaa 1.0 euroa", kortti.toString());
+    }
+    
+    public void saldoEiMeneNegatiiviseksi(){
+        kortti.otaRahaa(11);
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
+    
+    public void otaRahaaPalauttaaTrue(){
+        assertEquals(true, kortti.otaRahaa(9));
+    }
+    
+    public void otaRahaaPalauttaaFalse(){
+        assertEquals(true, kortti.otaRahaa(11));
+    }
+    
 
     @Test
     public void luotuKorttiOlemassa() {
